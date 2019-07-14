b473bf3fd85210cb5427ceafe2ef5770efdaf90a
Jesse McKinney <jessman51386@gmail.com>
2019-07-14 16:47:13 -0400

    got everything going but, cant get the time crap to work???

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,159 @@
-var config = {
-    apiKey: "AIzaSyAaoKp0Xod_gObg2BIlujtAC5yg4kuMFUQ",
-    authDomain: "traintime-ba953.firebaseapp.com",
-    databaseURL: "https://traintime-ba953.firebaseio.com",
-    storageBucket: "traintime-ba953.appspot.com"
-};
-////////////////////////////////////////////////////////////////
-// var firebaseConfig = {                                     //
-//     apiKey: "AIzaSyAaoKp0Xod_gObg2BIlujtAC5yg4kuMFUQ",     //
-//     authDomain: "traintime-ba953.firebaseapp.com",         //
-//     databaseURL: "https://traintime-ba953.firebaseio.com", //
-//     projectId: "traintime-ba953",                          //
-//     storageBucket: "traintime-ba953.appspot.com",          //
-//     messagingSenderId: "117555075753",                     //
-//     appId: "1:117555075753:web:cee20a606b84d593"           //
-//   };                                                       //
-////////////////////////////////////////////////////////////////
-firebase.initializeApp(config);
-var database = firebase.database();
+$(document).ready(function () {
 
-var trainName = "";
-var destination = "";
-var trainTime = "";
-var frequency = "";
+    var config = {
+        apiKey: "AIzaSyAaoKp0Xod_gObg2BIlujtAC5yg4kuMFUQ",
+        authDomain: "traintime-ba953.firebaseapp.com",
+        databaseURL: "https://traintime-ba953.firebaseio.com",
+        storageBucket: "traintime-ba953.appspot.com",
+        messagingSenderId: "117555075753"
+    };
+    ////////////////////////////////////////////////////////////////
+    // var firebaseConfig = {                                     //
+    //     apiKey: "AIzaSyAaoKp0Xod_gObg2BIlujtAC5yg4kuMFUQ",     //
+    //     authDomain: "traintime-ba953.firebaseapp.com",         //
+    //     databaseURL: "https://traintime-ba953.firebaseio.com", //
+    //     projectId: "traintime-ba953",                          //
+    //     storageBucket: "traintime-ba953.appspot.com",          //
+    //     messagingSenderId: "117555075753",                     //
+    //     appId: "1:117555075753:web:cee20a606b84d593"           //
+    //   };                                                       //
+    ////////////////////////////////////////////////////////////////
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
-$("#submit").on("click", function (event) {
-    event.preventDefault();
+    var currentTime = moment();
+    $('#currentTime').text(currentTime);
 
-    console.log("pushed")
+    var trainName = "";
+    var destination = "";
+    var trainTime = "";
+    var frequency = 0;
 
-    trainName = $("#inputTrainName").val().trim();
-    destination = $("#inputDestination").val().trim();
-    trainTime = $("#inputTrainTime").val().trim();
-    frequency = $("#inputFrequency").val().trim();
+    // var trainName;
+    // var destination;
+    // var trainTime;
+    // var frequency = 0;
 
-    database.ref().push({
-        TrainName: trainName,
-        Destination: destination,
-        TrainTime: trainTime,
-        Frequency: frequency
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+
+        // console.log("pushed")
+
+        trainName = $("#inputTrainName").val().trim();
+        destination = $("#inputDestination").val().trim();
+        trainTime = $("#inputTrainTime").val().trim();
+        frequency = $("#inputFrequency").val().trim();
+
+        // console.log(trainName)
+        // console.log(destination)
+        // console.log(trainTime)
+        // console.log(frequency)
+
+        database.ref().push({
+            TrainName: trainName,
+            Destination: destination,
+            TrainTime: trainTime,
+            Frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("#inputTrainName").val("");
+        $("#inputDestination").val("");
+        $("#inputTrainTime").val("");
+        $("#inputFrequency").val("");
+
     });
 
-    console.log(trainName)
-    console.log(destination)
-    console.log(trainTime)
-    console.log(frequency)
+    database.ref().on("child_added", function (childSnapshot) {
+        // console.log(childSnapshot.val());
 
-    $("#trainName").val("");
-    $("#destination").val("");
-    $("#trainTime").val("");
-    $("#frequency").val("");
+        // trainName = childSnapshot.val().trainName;
+        // destination = childSnapshot.val().destination;
+        // var tFrequency = childSnapshot.val().frequency;
+        // var firstTime = childSnapshot.val().trainTime;
 
+        // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        // console.log(firstTimeConverted);
+
+        // var currentTime = moment();
+        // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // var tRemainder = diffTime % tFrequency;
+        // console.log(tRemainder);
+
+        // var tMinutesTillTrain = tFrequency - tRemainder;
+        // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        ///////////////////////////////////////////////
+
+        var frequency = parseInt(frequency);
+        var currentTime = moment();
+        // console.log("Current Time: " + moment().format("hh:mm"));
+        // console.log(frequency);
+        // console.log(currentTime);
+
+        var dateConvert = moment(childSnapshot.val().trainTime, "hh:mm").subtract(1, "years");
+        // console.log("Date converted: " + dateConvert);
+
+        var trainTime = moment(dateConvert).format("hh:mm");
+        // console.log("Train Time : " + trainTime);
+
+        var timeConvert = moment(trainTime, "hh:mm").subtract(1, "years");
+        var timeDiff = moment().diff(moment(timeConvert), "minutes");
+        // console.log("Difference in Times: " + timeDiff);
+        // console.log(timeConvert);
+
+        var timeRemain = timeDiff % frequency;
+        // console.log(timeRemain);
+
+        var minutesAway = frequency - timeRemain;
+        // console.log("Minutes until next Train: " + minutesAway);
+
+        var trainTime = moment().add(minutesAway, "minutes");
+        // console.log(trainTime);
+
+        // $("#currentTime").text(currentTime);
+
+        $("#trainTable").append(
+            "<tr><td>" + childSnapshot.val().train +
+            "</td><td>" + childSnapshot.val().destination +
+            "</td><td>" + childSnapshot.val().frequency +
+            "</td><td>" + moment(trainTime).format("hh:mm") +
+            "</td><td>" + minutesAway + "</td></tr>");
+    },
+
+        function (errorObject) {
+            console.log("Errors handled: " + errorObject.code)
+        });
+
+
+    /////////////////////////////////////////////////////////
+
+    // var firstTimeConverted = moment(nextArrivalTable, "hh:mm").subtract(1, "years");
+
+    // var currentTime = moment();
+
+    //     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    //     var tRemainder = diffTime % frequencyTable;
+
+    //     var minutesTillTrain = frequencyTable - tRemainder;
+    //     var nextTrain = moment().add(minutesTillTrain, "minutes");
+    //     var nextTrainFormatted = moment(nextTrain).format("hh:mm");
+
+    //     $("#train-table").append("<tr><td>"
+    //         + trainNameTable + "</td><td>"
+    //         + destinationTable + "</td><td>"
+    //         + frequencyTable + "</td><td>"
+    //         + nextTrainFormatted + "</td><td>"
+    //         + minutesTillTrain + "</td></tr>");
+
+    // }, function (errorObject) {
+    //     console.log("Errors handled: " + errorObject.code);
+    // })
 });
-
